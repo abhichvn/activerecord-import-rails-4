8afdd867497b1438ef837eed415d707d618ca810
Abhijit Chavan <chavan.abhijit@synechron.com>
2016-04-13 12:06:05 +0530

    changes

--- lib/activerecord-import/adapters/oracle_enhanced_adapter.rb
+++ lib/activerecord-import/adapters/oracle_enhanced_adapter.rb
@@ -1,7 +1,14 @@
 module ActiveRecord::Import::OracleEnhancedAdapter
   module InstanceMethods
+
+    def self.included(klass)
+      klass.instance_eval do
+        include ActiveRecord::Import::ImportSupport
+      end
+    end
+
     def next_value_for_sequence(sequence_name)
-		%{#{sequence_name}.nextval}
+		  %{#{sequence_name}.nextval}
     end
   end
 end
